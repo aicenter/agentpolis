fd7b7687848e6d6b8b731ad4931eba41d074cfc6
zdebou <bousa.zdenek@gmail.com>
2016-12-21 13:17:17 +0100

    adding lanes direction

---
+++ src/main/java/cz/agents/agentpolis/simmodel/environment/model/citymodel/transportnetwork/elements/LaneTurnDirection.java
@@ -0,0 +1,10 @@
+package cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.elements;
+
+import java.io.Serializable;
+
+/**
+ * @author Zdenek Bousa
+ */
+public interface LaneTurnDirection extends Serializable {
+
+}
