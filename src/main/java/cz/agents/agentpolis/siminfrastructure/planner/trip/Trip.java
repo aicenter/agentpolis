09fe63e84b7c6cbaea5b82b107753a109c7421bd
Martin Schaefer <martin.schaefer@fel.cvut.cz>
2017-05-18 13:23:20 +0200

    migration from Logger to agentpolis Log

--- src/main/java/cz/agents/agentpolis/siminfrastructure/planner/trip/Trip.java
+++ src/main/java/cz/agents/agentpolis/siminfrastructure/planner/trip/Trip.java
@@ -1,84 +1,81 @@
 package cz.agents.agentpolis.siminfrastructure.planner.trip;
 
+import cz.agents.agentpolis.siminfrastructure.Log;
+
 import java.util.LinkedList;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
- *
- * @author F.I.D.O.
  * @param <L> locationType
+ * @author F.I.D.O.
  */
 public class Trip<L> {
-	protected final LinkedList<L> locations;
+    protected final LinkedList<L> locations;
 
-	
-	
-	public LinkedList<L> getLocations() {
-		return locations;
-	}
-	
-	
-	
-	public Trip(LinkedList<L> locations){
-		try {
-			checkLocations(locations);
-		} catch (TripException ex) {
-			Logger.getLogger(Trip.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		this.locations = locations;
-	}
-	
-	public Trip(L startLocation, L endLocation){
-		if(startLocation == null || endLocation == null){
-			try {
-				throw new TripException();
-			} catch (TripException ex) {
-				Logger.getLogger(Trip.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-		
-		locations = new LinkedList<>();
+
+    public LinkedList<L> getLocations() {
+        return locations;
+    }
+
+
+    public Trip(LinkedList<L> locations) {
+        try {
+            checkLocations(locations);
+        } catch (TripException ex) {
+            Log.log(this, Level.SEVERE, ex.getMessage());
+        }
+        this.locations = locations;
+    }
+
+    public Trip(L startLocation, L endLocation) {
+        if (startLocation == null || endLocation == null) {
+            try {
+                throw new TripException();
+            } catch (TripException ex) {
+                Log.log(this, Level.SEVERE, ex.getMessage());
+            }
+        }
+
+        locations = new LinkedList<>();
         locations.add(startLocation);
         locations.add(endLocation);
-	}
-	
-	
-	
-	public void extendTrip(L location){
-		if(location == null){
-			try {
-				throw new TripException();
-			} catch (TripException ex) {
-				Logger.getLogger(Trip.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-		locations.addLast(location);
-	}
-	
-	public String locationsToString(){
-		String str = "";
-		
-		for (L location : locations) {
-			str += location.toString() + System.getProperty("line.separator");
-		}
-		
-		return str;
-	}
-    
-    public L getAndRemoveFirstLocation(){
+    }
+
+
+    public void extendTrip(L location) {
+        if (location == null) {
+            try {
+                throw new TripException();
+            } catch (TripException ex) {
+                Log.log(this, Level.SEVERE, ex.getMessage());
+            }
+        }
+        locations.addLast(location);
+    }
+
+    public String locationsToString() {
+        String str = "";
+
+        for (L location : locations) {
+            str += location.toString() + System.getProperty("line.separator");
+        }
+
+        return str;
+    }
+
+    public L getAndRemoveFirstLocation() {
         return locations.poll();
     }
- 
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return locations.isEmpty();
     }
 
-	private void checkLocations(LinkedList<L> locations) throws TripException {
-		for (L location : locations) {
-			if(location == null){
-				throw new TripException();
-			}
-		}
-	}
+    private void checkLocations(LinkedList<L> locations) throws TripException {
+        for (L location : locations) {
+            if (location == null) {
+                throw new TripException();
+            }
+        }
+    }
 }
