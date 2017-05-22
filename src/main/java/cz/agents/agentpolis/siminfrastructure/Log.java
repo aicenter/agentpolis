475a92877fe1351fad71f95bfb309bd619cf027e
Martin Schaefer <martin.schaefer@fel.cvut.cz>
2017-05-22 15:09:46 +0200

    refact, debug and logging after change from events to messages in activity logic

--- src/main/java/cz/agents/agentpolis/siminfrastructure/Log.java
+++ src/main/java/cz/agents/agentpolis/siminfrastructure/Log.java
@@ -12,72 +12,86 @@ import java.util.logging.Logger;
 
 /**
  * Logger,
+ *
  * @author david_000
  */
 public class Log {
-	
-	/**
-	 * Java logger.
-	 */
-	private static Logger logger;
-	
-	
-	
-	
-	/**
-	 * Inits logger.
-     * @param name Java log name
-	 * @param logLevel Java log level for logging to file.
+
+    /**
+     * Java logger.
+     */
+    private static Logger logger;
+
+
+    /**
+     * Inits logger.
+     *
+     * @param name        Java log name
+     * @param logLevel    Java log level for logging to file.
      * @param logFilePath path to log file
-	 */
-	public static void init(final String name, final Level logLevel, final String logFilePath){
-		logger = Logger.getLogger(name);
-		logger.setLevel(logLevel);
-        
+     */
+    public static void init(final String name, final Level logLevel, final String logFilePath) {
+        logger = Logger.getLogger(name);
+        logger.setLevel(logLevel);
+
         // do not send log messages to other logs
-		logger.setUseParentHandlers(false);
-        
+        logger.setUseParentHandlers(false);
+
         LogFormater logFormater = new LogFormater();
-		
-		// conslole log settings
-		ConsoleHandler consoleHandler = new ConsoleHandler();
-		consoleHandler.setLevel(Level.INFO);
-		consoleHandler.setFormatter(logFormater);
-		logger.addHandler(consoleHandler);
-		
-		try {  
-			// file log settings
-			FileHandler fileHandler = new FileHandler(logFilePath);
-			fileHandler.setLevel(logLevel);
-			fileHandler.setFormatter(logFormater);
-			logger.addHandler(fileHandler);
-		} catch (IOException ex) {
-			Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
-		} catch (SecurityException ex) {
-			Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
-	
-	/**
-	 * Log message with no params.
-	 * @param caller Caller of the method.
-	 * @param level Java log level.
-	 * @param message Log message.
-	 */
-    public static void log(Object caller, Level level, String message){ 
-        message = caller.getClass().getName() + ": " + message;
-		logger.log(level, message);
+
+        // console log settings
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.INFO);
+        consoleHandler.setFormatter(logFormater);
+        logger.addHandler(consoleHandler);
+
+        try {
+            // file log settings
+            FileHandler fileHandler = new FileHandler(logFilePath);
+            fileHandler.setLevel(logLevel);
+            fileHandler.setFormatter(logFormater);
+            logger.addHandler(fileHandler);
+        } catch (IOException ex) {
+            Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-	/**
-	 * Log message.
-	 * @param caller Caller of the method.
-	 * @param level Java log level.
-	 * @param message Log message.
-	 * @param params Message parameters.
-	 */
-    public static void log(Object caller, Level level, String message, Object... params){
+
+    /**
+     * Log message with no params.
+     *
+     * @param caller  Caller of the method.
+     * @param level   Java log level.
+     * @param message Log message.
+     */
+    public static void log(Object caller, Level level, String message) {
         message = caller.getClass().getName() + ": " + message;
-		logger.log(level, message, params);
+        logger.log(level, message);
+    }
+
+    /**
+     * Log message.
+     *
+     * @param caller  Caller of the method.
+     * @param level   Java log level.
+     * @param message Log message.
+     * @param params  Message parameters.
+     */
+    public static void log(Object caller, Level level, String message, Object... params) {
+        message = caller.getClass().getName() + ": " + message;
+        logger.log(level, message, params);
+    }
+
+    public static void info(Object caller, String msg) {
+        log(caller, Level.INFO, msg);
+    }
+
+    public static void warn(Object caller, String msg) {
+        log(caller, Level.WARNING, msg);
+    }
+
+    public static void debug(Object caller, String msg) {
+        log(caller, Level.FINE, msg);
     }
 }
