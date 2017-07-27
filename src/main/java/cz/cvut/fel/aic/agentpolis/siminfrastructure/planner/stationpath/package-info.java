f997d0c064c8e988b29dd4d99d6c6d192a5d00b9
david.fido.fiedler@gmail.com <david.fido.fiedler@gmail.com>
2017-07-27 12:49:29 +0200

    cz.cvut.fel.aic.agentpolis.siminfrastructure.planner.stationpath package removed

--- src/main/java/cz/cvut/fel/aic/agentpolis/siminfrastructure/planner/stationpath/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * The wrappers providing information about paths between two stations
- *    
- * 
- * @author Zbynek Moler
- */
-package cz.cvut.fel.aic.agentpolis.siminfrastructure.planner.stationpath;
-
