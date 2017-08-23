0e26b77e3b32e0ef3e37a4e6f7196117df05e0a9
Šimon Wernisch <wernisim@fel.cvut.cz>
2017-08-23 18:31:40 +0200

    Reworked OsmImageLayer

--- src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/OsmImageLayer.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/OsmImageLayer.java
@@ -1,11 +1,6 @@
 package cz.cvut.fel.aic.agentpolis.simulator.visualization.visio;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import cz.agents.alite.simulation.Simulation;
 import cz.agents.alite.vis.Vis;
 import cz.agents.alite.vis.layer.AbstractLayer;
@@ -13,88 +8,136 @@ import org.apache.log4j.Logger;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.HashMap;
-import java.util.Map;
 
-@Singleton
 public class OsmImageLayer extends AbstractLayer {
 
     private Logger LOGGER = Logger.getLogger(Simulation.class);
 
-    private static final double WORLD_X = 20026376.39;
-    private static final double WORLD_Y = 20048966.10;
+    private static final double WORLD_X = 20037509.92 * 2;
+    private static final double WORLD_Y = 20037508.34 * 2;
     private static final String OSM_TILES_ROOT = System.getProperty("user.home")+"/.GMapCatcher";
-    private static final String IMAGE_FILTER = ".png";
 
-    class OSMTile {
+    private final Path dir = Paths.get(OSM_TILES_ROOT + "/OSM_tiles");
 
-        int x,y;
-        int worldX, worldY;
-        BufferedImage image;
-
-        public OSMTile(int x, int y, BufferedImage image) {
-            this.x = x;
-            this.y = y;
-
-            this.image = image;
-        }
-    }
-    private final Multimap<Integer, OSMTile> OSMTiles;
+    private final HashMap<OsmKey, BufferedImage> OSMTiles;
+    private int zoomLevel;
+    private int minIDX, minIDY, maxIDX, maxIDY;
+    private int zoomW;
+    private int zoomH;
 
     @Inject
     public OsmImageLayer() {
-        Path dir = Paths.get(OSM_TILES_ROOT + "/OSM_tiles");
         if (!Files.isDirectory(dir)) {
             LOGGER.info("No OSM tile folder found.");
             OSMTiles = null;
             return;
         }
 
-        OSMTiles = LinkedListMultimap.create();
-
-        try {
-            Files.walk(dir)
-                    .filter(p -> p.toString().endsWith(IMAGE_FILTER))
-                    .forEach(this::getImage);
-        } catch (IOException e) {
-            LOGGER.warn("Error while walking tile directory tree.");
-        }
+        OSMTiles = new HashMap<>();
     }
 
-    private void getImage(Path p) {
-        int count = p.getNameCount();
-        String zoomS = p.getName(count-3).toString();
-        int zoom = Integer.parseInt(p.getName(count-3).toString());
-        int x = Integer.parseInt(p.getName(count-2).toString());
-        int y = Integer.parseInt(p.getFileName().toString().replaceAll(IMAGE_FILTER,""));
-        try {
+    private BufferedImage getTile(int zoom, int x, int y) {
+        OsmKey osmKey = new OsmKey(zoom, x, y);
+        if (OSMTiles.containsKey(osmKey)) return OSMTiles.get(osmKey);
+        Path p = dir.resolve(Paths.get(Integer.toString(zoom),Integer.toString(x),Integer.toString(y)+".png"));
+        if (Files.isRegularFile(p) && Files.isReadable(p)) try {
             BufferedImage img = ImageIO.read(p.toFile());
-            OSMTiles.put(zoom, new OSMTile(x,y, img));
+            OSMTiles.put(osmKey, img);
+            return img;
         } catch (IOException e) {
-            LOGGER.warn("Could not load image "+p);
+            LOGGER.warn("Could not open file "+p.toString());
         }
+        OSMTiles.put(osmKey,null);
+        return  null;
     }
 
     @Override
     public void paint(Graphics2D canvas) {
         if (OSMTiles == null) return;
 
-        //Dimension dim = Vis.getDrawingDimension();
-        int zoomLevel = 16;
-        //Rectangle2D drawingRectangle = new Rectangle(dim); //pixel
-        //double invX = Vis.transInvX((int) drawingRectangle.getX());// pixel -> world
-        //double invY = Vis.transInvY((int) drawingRectangle.getY());
-        for (OSMTile t : OSMTiles.get(zoomLevel)) {
-            int tileOrigX = Vis.transX(t.x*(WORLD_X*2d/(1<<zoomLevel))-WORLD_X);
-            int tileOrigY = Vis.transY(-t.y*(WORLD_Y*2d/(1<<zoomLevel))+WORLD_Y);
-            int tileZoomW = Vis.transW(WORLD_X*2d/(1<<zoomLevel));
-            int tileZoomH = Vis.transH(WORLD_Y*2d/(1<<zoomLevel));
-            canvas.drawImage(t.image, tileOrigX,tileOrigY,
-                    tileZoomW,tileZoomH, null);
+        // get screen dimensions
+        Dimension dim = Vis.getDrawingDimension();
+        Rectangle drawingRectangle = new Rectangle(dim);
+
+        // calculate zoomed image sizes and level modifier
+        zoomW = Vis.transW(WORLD_X / (1 << zoomLevel));
+        zoomH = Vis.transH(WORLD_Y / (1 << zoomLevel));
+        int zoomLevelModifier = (int) log2(zoomW/256d);
+        // modify and bind zoomLevel and recalculate zoomed image sizes
+        if (zoomLevelModifier != 0) {
+            zoomLevel += zoomLevelModifier;
+            if (zoomLevel > 17) zoomLevel=17;
+            else if (zoomLevel < 0) zoomLevel=0;
+            zoomW = Vis.transW(WORLD_X/(1<<zoomLevel));
+            zoomH = Vis.transH(WORLD_Y/(1<<zoomLevel));
+        }
+
+        // calculate tileID bounds for current zoomLevel and screen dimensions
+        minIDX = worldToSlippyX(Vis.transInvX(0));
+        minIDY = worldToSlippyY(Vis.transInvY(0));
+        maxIDX = worldToSlippyX(Vis.transInvX((int) drawingRectangle.getMaxX())) + 1;
+        maxIDY = worldToSlippyY(Vis.transInvY((int) drawingRectangle.getMaxY())) + 1;
+
+        // finally draw tiles
+        drawImages(canvas);
+    }
+
+    private void drawImages(Graphics2D graphics) {
+        graphics.setColor(Color.BLACK);
+        for (int i = minIDX; i < maxIDX; i++) {
+            for (int j = minIDY; j < maxIDY; j++) {
+                BufferedImage bufferedImage = getTile(zoomLevel,i,j);
+                if (bufferedImage != null) graphics.drawImage(bufferedImage,
+                        Vis.transX(SlippyToWorldX(i)),Vis.transY(SlippyToWorldY(j)),
+                        zoomW,zoomH,null);
+            }
+        }
+    }
+
+    private int worldToSlippyX(double d) {
+        return (int)((d+WORLD_X/2)*(1<<zoomLevel)/WORLD_X);
+    }
+
+    private int worldToSlippyY(double d) { return (int)(-1*(d-WORLD_Y/2)*(1<<zoomLevel)/WORLD_Y); }
+
+    private double SlippyToWorldX(int i) {
+        return i*(WORLD_X/(1<<zoomLevel))-WORLD_X/2;
+    }
+
+    private double SlippyToWorldY(int i) { return -i*(WORLD_Y/(1<<zoomLevel))+WORLD_Y/2; }
+
+    private static double log2(double num) { return (Math.log(num)/Math.log(2)); }
+
+    class OsmKey {
+
+        private final int zoom, x, y;
+
+        OsmKey(int zoom, int x, int y) {
+            this.zoom = zoom;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            OsmKey osmKey = (OsmKey) o;
+
+            return zoom == osmKey.zoom && x == osmKey.x && y == osmKey.y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = zoom;
+            result = 31 * result + x;
+            result = 31 * result + y;
+            return result;
         }
     }
 }
