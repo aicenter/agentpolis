af05aa85ec065bce1dedfd9ccfe89204b65de2b3
david.fido.fiedler@gmail.com <david.fido.fiedler@gmail.com>
2017-10-10 15:58:17 +0200

    redundant config properties removed

--- src/main/java/cz/cvut/fel/aic/agentpolis/config/AgentpolisConfig.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/config/AgentpolisConfig.java
@@ -7,12 +7,6 @@ import java.util.Map;
 import ninja.fido.config.GeneratedConfig;
 
 public class AgentpolisConfig implements GeneratedConfig {
-  public Boolean showEventViewer;
-
-  public String pathToScriptsAndTheirInputParameters;
-
-  public String dirForResults;
-
   public CongestionModel congestionModel;
 
   public Integer simulationDurationInMillis;
@@ -25,8 +19,6 @@ public class AgentpolisConfig implements GeneratedConfig {
 
   public String log4jXmlDir;
 
-  public String pathToCsvEventLogFile;
-
   public Boolean skipSimulation;
 
   public Integer srid;
@@ -35,16 +27,12 @@ public class AgentpolisConfig implements GeneratedConfig {
   }
 
   public AgentpolisConfig fill(Map agentpolisConfig) {
-    this.showEventViewer = (Boolean) agentpolisConfig.get("show_event_viewer");
-    this.pathToScriptsAndTheirInputParameters = (String) agentpolisConfig.get("path_to_scripts_and_their_input_parameters");
-    this.dirForResults = (String) agentpolisConfig.get("dir_for_results");
     this.congestionModel = new CongestionModel((Map) agentpolisConfig.get("congestion_model"));
     this.simulationDurationInMillis = (Integer) agentpolisConfig.get("simulation_duration_in_millis");
     this.showVisio = (Boolean) agentpolisConfig.get("show_visio");
     this.pathToMapTiles = (String) agentpolisConfig.get("path_to_map_tiles");
     this.osmTileServer = (String) agentpolisConfig.get("osm_tile_server");
     this.log4jXmlDir = (String) agentpolisConfig.get("log4j_xml_dir");
-    this.pathToCsvEventLogFile = (String) agentpolisConfig.get("path_to_csv_event_log_file");
     this.skipSimulation = (Boolean) agentpolisConfig.get("skip_simulation");
     this.srid = (Integer) agentpolisConfig.get("srid");
     return this;
