db67a0832e303b734138c642a34deac830b5a25c
zdebou <bousa.zdenek@gmail.com>
2017-11-27 17:20:33 +0100

    Support of lanes in visio

--- src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/entity/vehicle/Vehicle.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/entity/vehicle/Vehicle.java
@@ -11,19 +11,20 @@ import cz.cvut.fel.aic.agentpolis.simmodel.entity.AgentPolisEntity;
 import cz.cvut.fel.aic.agentpolis.simmodel.entity.MovingEntity;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
 
-/**
- * @author fido
- */
 public abstract class Vehicle extends AgentPolisEntity implements MovingEntity {
-
     private Driver driver;
-
     private double queueBeforeVehicleLength;
-
     private SimulationNode lastFromPosition;
+    private long laneId = -1; // lana id == congestion lane and lane id
 
-    public SimulationNode getLastFromPosition() {
-        return lastFromPosition;
+    /**
+     * Vehicle
+     *
+     * @param id
+     * @param position
+     */
+    public Vehicle(String id, SimulationNode position) {
+        super(id, position);
     }
 
     @Override
@@ -36,19 +37,27 @@ public abstract class Vehicle extends AgentPolisEntity implements MovingEntity {
         return ((driver != null) ? driver.getDelayData() : null);
     }
 
+    @Override
+    public void setPosition(SimulationNode position) {
+        super.setPosition(position);
+        if (driver != null) {
+            driver.setPosition(position);
+        }
+    }
+
+    public SimulationNode getLastFromPosition() {
+        return lastFromPosition;
+    }
+
     public void setLastFromPosition(SimulationNode lastTargetPosition) {
         this.lastFromPosition = lastTargetPosition;
     }
 
-
-    public Driver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(Driver driver) {
-        this.driver = driver;
-    }
-
+    /**
+     * Que
+     *
+     * @return
+     */
     public double getQueueBeforeVehicleLength() {
         return queueBeforeVehicleLength;
     }
@@ -57,17 +66,31 @@ public abstract class Vehicle extends AgentPolisEntity implements MovingEntity {
         this.queueBeforeVehicleLength = queueBeforeVehicleLength;
     }
 
-
-    public Vehicle(String id, SimulationNode position) {
-        super(id, position);
+    /**
+     * Driver
+     */
+    public Driver getDriver() {
+        return driver;
     }
 
-    @Override
-    public void setPosition(SimulationNode position) {
-        super.setPosition(position);
-        if (driver != null) {
-            driver.setPosition(position);
-        }
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * ID of lane where the vehicle is
+     *
+     * @param id - lane id or congestion lane id, laneId == congestionlaneId
+     */
+    public void setLaneId(long id) {
+        this.laneId = id;
+    }
+
+    /**
+     * @return unique id, it is possible to retrieve lane or congestionLane. -1 for unknown situation.
+     */
+    public long getLaneId() {
+        return this.laneId;
     }
 
 
