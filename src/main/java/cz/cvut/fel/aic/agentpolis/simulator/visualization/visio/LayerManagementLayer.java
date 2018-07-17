39070e378745d06dede407d8238f60bee85f895a
Michal Cvach <michal.cvach@seznam.cz>
2018-07-17 19:10:23 +0200

    Prepared UI for changing layers order, the logic isn't done yet though

--- src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/LayerManagementLayer.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/LayerManagementLayer.java
@@ -41,6 +41,7 @@ public class LayerManagementLayer extends AbstractLayer {
 
     @Override
     public void paint(Graphics2D canvas) {
+        canvas.setStroke(new BasicStroke(1));
         if (isVisible) {
             paintBottomRectangle(canvas, "Hide layers management");
             paintArrowDownwards(canvas);
@@ -75,9 +76,11 @@ public class LayerManagementLayer extends AbstractLayer {
             canvas.drawString(manageableLayers.get(i).getName(), 1630, 935 - 25*i);
             if (manageableLayers.get(i).isVisible()){
                 canvas.fillRect(1602,921 - 25*i, 18, 18);
+
             } else {
                 canvas.drawRect(1602,921 - 25*i, 18, 18);
             }
+            paintLayerOrderArrows(canvas, 921-25*i);
         }
         canvas.setFont(oldFont);
 
@@ -95,26 +98,62 @@ public class LayerManagementLayer extends AbstractLayer {
         canvas.fillPolygon(xPoints, yPoints, 3);
     }
 
+    public void paintLayerOrderArrows(Graphics2D canvas, int ypos) {
+        int[] xPoints = {1585, 1591, 1597};
+        int[] yPointsUp = {ypos+8, ypos, ypos+8};
+        int[] yPointsDown = {ypos+10, ypos+18, ypos+10};
+        canvas.fillPolygon(xPoints, yPointsUp, 3);
+        canvas.fillPolygon(xPoints, yPointsDown, 3);
+    }
+
+
     private void setDefaultColors() {
         backgroundColor = new Color(0, 0, 0, 170);
         textColor = new Color(255, 255, 255, 255);
     }
 
-    private void validateLayersManagementToggleClick(Point2d click) {
+    private boolean validateLayersManagementToggleClick(Point2d click) {
         if (click.getX() > 1580 && click.getX() < 1880 && click.getY() > 955 && click.getY() < 985 ) {
             isVisible = !isVisible;
+            return true;
         }
+        return false;
     }
 
-    private void validateLayerToggleClick(Point2d click) {
+    private boolean validateLayerToggleClick(Point2d click) {
         if (click.getX() > 1602 && click.getX() < 1620 && click.getY() > 921 - 25*manageableLayers.size() && click.getY() < 939 ) {
             for (int i = 0; i < manageableLayers.size(); i++) {
-                if (click.getX() > 1602 && click.getX() < 1620 && click.getY() > 921 - 25*i && click.getY() < 939 - 25*i ) {
+                if (click.getY() > 921 - 25*i && click.getY() < 939 - 25*i ) {
                     manageableLayers.get(i).toggle();
-                    break;
+                    return true;
                 }
             }
         }
+        return false;
+    }
+
+    private boolean validateLayerMoveUpClick(Point2d click) {
+        if (click.getX() > 1585 && click.getX() < 1597 && click.getY() > 921 - 25*manageableLayers.size() && click.getY() < 939 ) {
+            for (int i = 0; i < manageableLayers.size(); i++) {
+                if (click.getY() > 921 - 25*i && click.getY() < 929 - 25*i ) {
+                    System.out.println("Move Up: " +  i);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean validateLayerMoveDownClick(Point2d click) {
+        if (click.getX() > 1585 && click.getX() < 1597 && click.getY() > 921 - 25*manageableLayers.size() && click.getY() < 939 ) {
+            for (int i = 0; i < manageableLayers.size(); i++) {
+                if (click.getY() > 931 - 25*i && click.getY() < 939 - 25*i ) {
+                    System.out.println("Move Down: " +  i);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void init(Vis vis) {
@@ -125,9 +164,16 @@ public class LayerManagementLayer extends AbstractLayer {
                 if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
                     Point2d click = new Point2d(mouseEvent.getX(), mouseEvent.getY());
 
-                    validateLayersManagementToggleClick(click);
-                    validateLayerToggleClick(click);
-
+                    if (validateLayersManagementToggleClick(click)){
+                        return;
+                    }
+                    if (validateLayerToggleClick(click)){
+                        return;
+                    }
+                    if(validateLayerMoveUpClick(click)){
+                        return;
+                    }
+                    validateLayerMoveDownClick(click);
                 }
             }
 
