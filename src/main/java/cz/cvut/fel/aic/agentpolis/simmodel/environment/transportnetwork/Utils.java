64ec4559e533926f484a87d19065908c0d38e94f
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2018-10-19 16:05:58 +0200

    Utils extended: - new method getGridGraph - getCompleteGraph method has new overload with custom radius

--- src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/environment/transportnetwork/Utils.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/environment/transportnetwork/Utils.java
@@ -31,10 +31,18 @@ import java.util.Arrays;
  * @author fido
  */
 public class Utils {
-    public static Graph<SimulationNode, SimulationEdge> getCompleteGraph(int nodeCount, Transformer projection) {
+	
+	private static final int DEFAULT_RADIUS = 500;
+	
+	private static final int DEFAULT_GRID_SIZE = 200;
+	
+	public static Graph<SimulationNode, SimulationEdge> getCompleteGraph(int nodeCount, Transformer projection){
+		return getCompleteGraph(nodeCount, projection, DEFAULT_RADIUS);
+	}
+	
+    public static Graph<SimulationNode, SimulationEdge> getCompleteGraph(int nodeCount, Transformer projection, 
+			int radius) {
         GraphBuilder<SimulationNode, SimulationEdge> graphBuilder = new GraphBuilder<>();
-
-        int radius = 1000;
 
         for (int i = 0; i < nodeCount; i++) {
             double angle = 2 * Math.PI / nodeCount * i;
@@ -60,4 +68,52 @@ public class Utils {
 
         return graphBuilder.createGraph();
     }
+	
+	public static Graph<SimulationNode, SimulationEdge> getGridGraph(int width, Transformer projection) {
+		return getGridGraph(width, projection, width);
+	}
+	
+	public static Graph<SimulationNode, SimulationEdge> getGridGraph(int width, Transformer projection, 
+			int height) {
+        GraphBuilder<SimulationNode, SimulationEdge> graphBuilder = new GraphBuilder<>();
+		
+		int size = DEFAULT_GRID_SIZE;
+
+        for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+
+				int x = 100 * j * size;
+				int y = 100 * i * size;
+				int nodeId = i * width + j;
+
+				SimulationNode node = new SimulationNode(nodeId, 0, y, x, 0, projection);
+				graphBuilder.addNode(node);
+				
+				// bottom edges
+				if(i != 0){
+					int bottomNodeId = nodeId - width;
+					createEdges(nodeId, bottomNodeId, graphBuilder);
+				}
+				
+				// left edges
+				if(j != 0){
+					int leftNodeId = nodeId - 1;
+					createEdges(nodeId, leftNodeId, graphBuilder);
+				}
+				
+			}
+        }
+
+        return graphBuilder.createGraph();
+    }
+	
+	private static void createEdges(int id1, int id2, GraphBuilder<SimulationNode, SimulationEdge> graphBuilder){
+		SimulationEdge edge1 = new SimulationEdge(graphBuilder.getNode(id1), graphBuilder.getNode(id2), 0, 0, 0, 100, 
+				40, 1, new EdgeShape(Arrays.asList(graphBuilder.getNode(id1), graphBuilder.getNode(id2))));
+		SimulationEdge edge2 = new SimulationEdge(graphBuilder.getNode(id2), graphBuilder.getNode(id1), 0, 0, 0, 100, 
+				40, 1, new EdgeShape(Arrays.asList(graphBuilder.getNode(id2), graphBuilder.getNode(id1))));
+
+		graphBuilder.addEdge(edge1);
+		graphBuilder.addEdge(edge2);
+	}
 }
