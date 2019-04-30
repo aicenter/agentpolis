58a7b4a0957bdb310111ddfb80887742925534fe
Marek Cuchý <marek.cuchy@agents.fel.cvut.cz>
2019-04-30 15:47:21 +0200

    refactored EntityLayer, VehicleLayer and PositionUtil to be more general (beside others) - version changed to 5.1.2-SNAPSHOT

--- src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/VehicleLayer.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simulator/visualization/visio/VehicleLayer.java
@@ -18,224 +18,156 @@
  */
 package cz.cvut.fel.aic.agentpolis.simulator.visualization.visio;
 
-import com.google.inject.Inject;
-
-import cz.cvut.fel.aic.agentpolis.config.AgentpolisConfig;
-import cz.cvut.fel.aic.alite.vis.Vis;
-import cz.cvut.fel.aic.agentpolis.simmodel.agent.Driver;
-import cz.cvut.fel.aic.agentpolis.simmodel.entity.TransportableEntity;
-import cz.cvut.fel.aic.agentpolis.simmodel.entity.vehicle.Vehicle;
-import cz.cvut.fel.aic.agentpolis.simmodel.environment.EntityStorage;
-import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
-
-
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Path2D;
-import java.util.ArrayList;
+import java.util.List;
+
 import javax.vecmath.Point2d;
 
+import cz.cvut.fel.aic.agentpolis.config.AgentpolisConfig;
+import cz.cvut.fel.aic.agentpolis.simmodel.entity.AgentPolisEntity;
+import cz.cvut.fel.aic.agentpolis.simmodel.entity.MovingEntity;
+import cz.cvut.fel.aic.agentpolis.simmodel.entity.TransportableEntity;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.EntityStorage;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
+import cz.cvut.fel.aic.alite.vis.Vis;
+
 /**
+ * //TODO: maybe rename to MovingEntityLayer + rename *vehicle* methods
  *
  * @author F-I-D-O
  */
-public abstract class VehicleLayer<V extends Vehicle>  extends EntityLayer<V>{
-    
-    private Path2D CarRepresentationShape;
-    
-   
-    public VehicleLayer(EntityStorage<V> driverStorage, AgentpolisConfig agentpolisConfig) {
-        this(driverStorage, agentpolisConfig.showStackedEntities, true);
-    }
-	
-    public VehicleLayer(EntityStorage<V> driverStorage, boolean showStackedEntitiesCount, boolean transformSize) {
-        super(driverStorage, showStackedEntitiesCount, transformSize);
-    }
+public abstract class VehicleLayer<V extends AgentPolisEntity & MovingEntity> extends EntityLayer<V> {
 
-    @Override
-    protected Point2d getEntityPosition(V vehicle) {
-        return positionUtil.getCanvasPositionInterpolatedForVehicle(vehicle);
-    }
+	private Path2D representativeShape;
 
-    @Override
-    protected Point2d getEntityPositionInTime(V vehicle, long time) {
-        return positionUtil.getCanvasPositionInterpolatedForVehicleInTime(vehicle, time);
-    }
+	public VehicleLayer(EntityStorage<V> driverStorage, AgentpolisConfig agentpolisConfig) {
+		this(driverStorage, agentpolisConfig.showStackedEntities, true);
+	}
 
-    //not used
-    @Override
-    protected int getEntityTransformableRadius(V vehicle) {
-        return 0;
-    }
-	
-	//not used
+	public VehicleLayer(EntityStorage<V> driverStorage, boolean showStackedEntitiesCount, boolean transformSize) {
+		super(driverStorage, showStackedEntitiesCount, transformSize);
+	}
+
+	@Override
+	protected Point2d getEntityPosition(V vehicle) {
+		return positionUtil.getCanvasPositionInterpolated(vehicle);
+	}
+
+	@Override
+	protected Point2d getEntityPositionInTime(V vehicle, long time) {
+		return positionUtil.getCanvasPositionInterpolatedInTime(vehicle, time);
+	}
+
+	// not used
+	@Override
+	protected int getEntityTransformableRadius(V vehicle) {
+		return 0;
+	}
+
+	// not used
 	@Override
 	protected double getEntityStaticRadius(V vehicle) {
-		return  0;
+		return 0;
 	}
-    
-    protected abstract float getVehicleWidth(V vehicle);
-    
-    protected abstract float getVehicleLength(V vehicle);
-	
+
+	protected abstract float getVehicleWidth(V vehicle);
+
+	protected abstract float getVehicleLength(V vehicle);
+
 	protected abstract float getVehicleStaticWidth(V vehicle);
-    
-    protected abstract float getVehicleStaticLength(V vehicle);
 
-    @Override
-    protected void drawEntities(ArrayList<V> entities, Point2d entityPosition, Graphics2D canvas, Dimension dim) {
-        V representative = entities.get(0);
-        
-        if(CarRepresentationShape == null){
-			if(transformSize){
-				CarRepresentationShape
-						= createCarShape(getVehicleLength(representative), getVehicleWidth(representative));
+	protected abstract float getVehicleStaticLength(V vehicle);
+
+	@Override
+	protected void drawEntities(List<V> entities, Point2d entityPosition, Graphics2D canvas, Dimension dim) {
+		V representative = entities.get(0);
+
+		Color color = getEntityDrawColor(representative);
+		canvas.setColor(color);
+
+		if (pointInside(entityPosition, dim)) {
+			drawEntityCount(entities,entityPosition, canvas, color);
+			drawEntityShape(representative, entityPosition, canvas);
+		}
+	}
+
+	protected void drawEntityCount(List<V> entities, Point2d entityPosition, Graphics2D canvas, Color color) {
+		int x = (int) (entityPosition.getX());
+		int y = (int) (entityPosition.getY());
+		if (entities.size() > 1) {
+			VisioUtils.printTextWithBackgroud(canvas, Integer.toString(entities.size()),
+					new Point((int) (x - getTextMarginTransX()), (int) (y - getTextMarginTransY())), color,
+					getTextBackgroundColor());
+		}
+	}
+
+	protected void drawEntityShape(V representative, Point2d entityPosition, Graphics2D canvas) {
+		double angle = getAngle(representative);
+		/* desired order of the transformations */
+		/* rotate against center of the shape */
+		AffineTransform rotate = AffineTransform.getRotateInstance(-angle, 0, 0);
+		/* translate the center of the shape to entity position */
+		AffineTransform translate = AffineTransform.getTranslateInstance(entityPosition.getX(),
+				entityPosition.getY());
+
+		/* transformations are applied in inverse order */
+		if (transformSize) {
+
+			/* scale according to zoom factor */
+			AffineTransform scale = AffineTransform.getScaleInstance(Math.max(1, Vis.getZoomFactor()),
+					Math.max(1, Vis.getZoomFactor()));
+
+			scale.concatenate(rotate);
+			translate.concatenate(scale);
+		} else {
+			translate.concatenate(rotate);
+		}
+		Shape s = getShape(representative).createTransformedShape(translate);
+		canvas.fill(s);
+	}
+
+	protected double getAngle(V representative) {
+		SimulationNode target = getTargetNode(representative);
+		double angle = 0;
+		if (target != null) {
+			angle = positionUtil.getAngle(representative);
+		}
+		return angle;
+	}
+
+	protected SimulationNode getTargetNode(V representative) {
+		if (representative != null && representative.getTargetNode() != null) {
+			return representative.getTargetNode();
+		} else if (representative instanceof TransportableEntity
+				&& ((TransportableEntity) representative).getTransportingEntity() != null) {
+			return ((TransportableEntity) representative).getTransportingEntity().getDriver().getTargetNode();
+		} else {
+			return representative.getPosition();
+		}
+	}
+
+	protected Path2D getShape(V representative) {
+		if (representativeShape == null) {
+			if (transformSize) {
+				representativeShape = createCarShape(getVehicleLength(representative), getVehicleWidth(representative));
+			} else {
+				representativeShape = createCarShape(getVehicleStaticLength(representative),
+						getVehicleStaticWidth(representative));
 			}
-			else{
-				CarRepresentationShape
-						= createCarShape(getVehicleStaticLength(representative), getVehicleStaticWidth(representative));
-			}
-        }
-        
-        Color color = getEntityDrawColor(entities.get(0));
-        canvas.setColor(color);
+		}
+		return representativeShape;
+	}
 
-        int x = (int) (entityPosition.getX());
-        int y = (int) (entityPosition.getY());
-        
-        if (x > 0 && x < dim.width && y > 0 && y < dim.height) {
-            
-            /* entity count text */
-            if(entities.size() > 1){
-                VisioUtils.printTextWithBackgroud(canvas, Integer.toString(entities.size()), 
-                    new Point((int) (x - getTextMarginTransX()), (int) (y - getTextMarginTransY())), color, 
-                    getTextBackgroundColor());
-            }
-            
-            double angle = 0;
-            SimulationNode target = null;
-            SimulationNode position = representative.getPosition();
-            Driver driver = representative.getDriver();
-            if(driver != null && driver.getTargetNode() != null){
-                target = driver.getTargetNode();
-            }
-            else if(representative instanceof TransportableEntity 
-                    && ((TransportableEntity) representative).getTransportingEntity() != null){
-                target = ((TransportableEntity) representative).getTransportingEntity().getDriver().getTargetNode();
-            }
-            else{
-                position = representative.getLastFromPosition();
-                target = representative.getPosition();
-            }
-            
-            if(target != null && position != null){
-                angle = positionUtil.getAngle(driver);
-            }
-            
-            /* desired order of the transformations */
-            
-            /* rotate against center of the shape */
-            AffineTransform rotate = AffineTransform.getRotateInstance(-angle, 0, 0);
-            
-            /* translate the center of the shape to entity position */
-            AffineTransform translate = 
-                    AffineTransform.getTranslateInstance(entityPosition.getX(), entityPosition.getY());
-            
-            /* transformations are applied in inverse order */
-			if(transformSize){
-				
-				/* scale according to zoom factor */
-				AffineTransform scale = AffineTransform.getScaleInstance(Math.max(1,Vis.getZoomFactor()),Math.max(1,Vis.getZoomFactor()));
+	public static Path2D createCarShape(final float length, final float width) {
+		final GeneralPath p0 = new GeneralPath();
+		p0.moveTo(-length / 2, -width / 2);
+		p0.lineTo(length / 2, 0);
+		p0.lineTo(-length / 2, width / 2);
+		p0.closePath();
+		return p0;
+	}
 
-				scale.concatenate(rotate);
-				translate.concatenate(scale);
-			}
-			else{
-				translate.concatenate(rotate);
-			}
-			
-            Shape s = CarRepresentationShape.createTransformedShape(translate);
-            canvas.fill(s);
-        }
-    }
-
-    @Override
-    protected void drawEntity(V representative, Point2d entityPosition, Graphics2D canvas, Dimension dim) {
-        if(CarRepresentationShape == null){
-            if(transformSize){
-                CarRepresentationShape
-                        = createCarShape(getVehicleLength(representative), getVehicleWidth(representative));
-            }
-            else{
-                CarRepresentationShape
-                        = createCarShape(getVehicleStaticLength(representative), getVehicleStaticWidth(representative));
-            }
-        }
-
-        Color color = getEntityDrawColor(representative);
-        canvas.setColor(color);
-
-        int x = (int) (entityPosition.getX());
-        int y = (int) (entityPosition.getY());
-
-        if (x > 0 && x < dim.width && y > 0 && y < dim.height) {
-
-            double angle = 0;
-            SimulationNode target = null;
-            SimulationNode position = representative.getPosition();
-            Driver driver = representative.getDriver();
-            if(driver != null && driver.getTargetNode() != null){
-                target = driver.getTargetNode();
-            }
-            else if(representative instanceof TransportableEntity
-                    && ((TransportableEntity) representative).getTransportingEntity() != null){
-                target = ((TransportableEntity) representative).getTransportingEntity().getDriver().getTargetNode();
-            }
-            else{
-                position = representative.getLastFromPosition();
-                target = representative.getPosition();
-            }
-
-            if(target != null && position != null){
-                angle = positionUtil.getAngle(driver);
-            }
-
-            /* desired order of the transformations */
-
-            /* rotate against center of the shape */
-            AffineTransform rotate = AffineTransform.getRotateInstance(-angle, 0, 0);
-
-            /* translate the center of the shape to entity position */
-            AffineTransform translate =
-                    AffineTransform.getTranslateInstance(entityPosition.getX(), entityPosition.getY());
-
-            /* transformations are applied in inverse order */
-            if(transformSize){
-
-                /* scale according to zoom factor */
-                AffineTransform scale = AffineTransform.getScaleInstance(Math.max(1,Vis.getZoomFactor()),Math.max(1,Vis.getZoomFactor()));
-
-                scale.concatenate(rotate);
-                translate.concatenate(scale);
-            }
-            else{
-                translate.concatenate(rotate);
-            }
-
-            Shape s = CarRepresentationShape.createTransformedShape(translate);
-            canvas.fill(s);
-        }
-    }
-    
-    private static Path2D createCarShape(final float length, final float width) {
-        final GeneralPath p0 = new GeneralPath();
-        p0.moveTo(-length /2, -width / 2);
-        p0.lineTo(length / 2, 0);
-        p0.lineTo(-length / 2, width / 2);
-        p0.closePath();
-        return p0;
-    }
-
-	
 }
