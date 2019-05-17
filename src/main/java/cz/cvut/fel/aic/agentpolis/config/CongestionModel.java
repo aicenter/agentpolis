975f7a690cbb56371bc22d01a88cdfb0696e0a28
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-05-17 18:30:37 +0200

    spaces replaced with tabs

--- src/main/java/cz/cvut/fel/aic/agentpolis/config/CongestionModel.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/config/CongestionModel.java
@@ -17,10 +17,10 @@ public class CongestionModel {
   public Integer defaultCrossroadDrivingLanes;
 
   public CongestionModel(Map congestionModel) {
-	this.randomSeed = (Integer) congestionModel.get("random_seed");
-	this.maxFlowPerLane = (Double) congestionModel.get("max_flow_per_lane");
-	this.batchSize = (Integer) congestionModel.get("batch_size");
-	this.fundamentalDiagramDelay = (Boolean) congestionModel.get("fundamental_diagram_delay");
-	this.defaultCrossroadDrivingLanes = (Integer) congestionModel.get("default_crossroad_driving_lanes");
+    this.randomSeed = (Integer) congestionModel.get("random_seed");
+    this.maxFlowPerLane = (Double) congestionModel.get("max_flow_per_lane");
+    this.batchSize = (Integer) congestionModel.get("batch_size");
+    this.fundamentalDiagramDelay = (Boolean) congestionModel.get("fundamental_diagram_delay");
+    this.defaultCrossroadDrivingLanes = (Integer) congestionModel.get("default_crossroad_driving_lanes");
   }
 }
--- src/main/java/cz/cvut/fel/aic/agentpolis/config/SimulationDuration.java
+++ src/main/java/cz/cvut/fel/aic/agentpolis/config/SimulationDuration.java
@@ -13,9 +13,9 @@ public class SimulationDuration {
   public Integer days;
 
   public SimulationDuration(Map simulationDuration) {
-	this.hours = (Integer) simulationDuration.get("hours");
-	this.seconds = (Integer) simulationDuration.get("seconds");
-	this.minutes = (Integer) simulationDuration.get("minutes");
-	this.days = (Integer) simulationDuration.get("days");
+    this.hours = (Integer) simulationDuration.get("hours");
+    this.seconds = (Integer) simulationDuration.get("seconds");
+    this.minutes = (Integer) simulationDuration.get("minutes");
+    this.days = (Integer) simulationDuration.get("days");
   }
 }
