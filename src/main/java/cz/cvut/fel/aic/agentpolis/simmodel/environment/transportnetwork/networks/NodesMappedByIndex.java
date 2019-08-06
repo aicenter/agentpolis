d915cc769fdb9052b722912ffd2fc0a90cdfe279
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-08-06 17:41:55 +0200

    adj matrix computation moved to amodsim, new nodes mapeed by index provider

---
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/environment/transportnetwork/networks/NodesMappedByIndex.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 Czech Technical University in Prague.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.networks;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author david
+ */
+@Singleton
+public class NodesMappedByIndex {
+	
+	private final Map<Integer,SimulationNode> nodesMappedByIndex;
+
+	@Inject
+	public NodesMappedByIndex(AllNetworkNodes nodes) {
+		nodesMappedByIndex = new HashMap<>();
+		for(SimulationNode node: nodes.getAllNetworkNodes().values()){
+			nodesMappedByIndex.put(node.getIndex(), node);
+		}
+	}
+	
+	public SimulationNode getNodeByIndex(int index){
+		return nodesMappedByIndex.get(index);
+	}
+	
+}
