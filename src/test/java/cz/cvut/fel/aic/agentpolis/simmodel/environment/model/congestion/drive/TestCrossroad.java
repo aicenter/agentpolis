c5b7fc49104707b9176b41326b5dddbea871350f
Travnicek Jan <travnja5@fel.cvut.cz>
2020-08-12 16:55:33 +0200

    Creating graph using Utils.getGridGraph

--- src/test/java/cz/cvut/fel/aic/agentpolis/simmodel/environment/model/congestion/drive/TestCrossroad.java
+++ src/test/java/cz/cvut/fel/aic/agentpolis/simmodel/environment/model/congestion/drive/TestCrossroad.java
@@ -21,11 +21,13 @@ package cz.cvut.fel.aic.agentpolis.simmodel.environment.model.congestion.drive;
 import cz.cvut.fel.aic.agentpolis.VisualTests;
 import cz.cvut.fel.aic.agentpolis.siminfrastructure.planner.trip.Trip;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.model.congestion.drive.support.DriveTest;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.Utils;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.EdgeShape;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationEdge;
 import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
 import cz.cvut.fel.aic.geographtools.Graph;
 import cz.cvut.fel.aic.geographtools.GraphBuilder;
+import cz.cvut.fel.aic.geographtools.util.Transformer;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -37,33 +39,46 @@ public class TestCrossroad {
 
 	@Test
 	public void run() {
-		GraphBuilder<SimulationNode, SimulationEdge> graphBuilder = new GraphBuilder<>();
+//		GraphBuilder<SimulationNode, SimulationEdge> graphBuilder = new GraphBuilder<>();
+//                int GRID = 100000;
+//                
+//                SimulationNode node0 = new SimulationNode(0, 0, 0, 0, 0, 0, 0, 0);
+//		SimulationNode node1 = new SimulationNode(1, 0, 0, 0, 0, GRID, 0, 0);
+//		SimulationNode node2 = new SimulationNode(2, 0, 0, 0, GRID, GRID, 0, 0);
+//		SimulationNode node3 = new SimulationNode(3, 0, 0, 0, 0, 2*GRID, 0, 0);
+//
+//		graphBuilder.addNode(node0);
+//		graphBuilder.addNode(node1);
+//		graphBuilder.addNode(node2);
+//		graphBuilder.addNode(node3);
+//
+//		SimulationEdge edge1 = new SimulationEdge(node0, node1, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node0, node1)));
+//		SimulationEdge edge2 = new SimulationEdge(node1, node0, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node1, node0)));
+//		SimulationEdge edge3 = new SimulationEdge(node1, node2, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node1, node2)));
+//		SimulationEdge edge4 = new SimulationEdge(node2, node1, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node2, node1)));
+//		SimulationEdge edge5 = new SimulationEdge(node1, node3, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node1, node3)));
+//		SimulationEdge edge6 = new SimulationEdge(node3, node1, 0, 0, GRID, 40, 1, new EdgeShape(Arrays.asList(node3, node1)));
+//
+//		graphBuilder.addEdge(edge1);
+//		graphBuilder.addEdge(edge2);
+//		graphBuilder.addEdge(edge3);
+//		graphBuilder.addEdge(edge4);
+//		graphBuilder.addEdge(edge5);
+//		graphBuilder.addEdge(edge6);
+//
+//		Graph<SimulationNode, SimulationEdge> graph = graphBuilder.createGraph();
+                
+                    
+                Transformer projection = new Transformer(26912);
+                Graph<SimulationNode, SimulationEdge> graph = Utils.getGridGraph(3, projection, 2);
+                
+                
+                SimulationNode node0 = graph.getNode(0);
+                SimulationNode node1 = graph.getNode(1);
+                SimulationNode node2 = graph.getNode(2);
+                SimulationNode node3 = graph.getNode(4);
+                
 
-		SimulationNode node0 = new SimulationNode(0, 0, 0, 0, 0, 0, 0, 0);
-		SimulationNode node1 = new SimulationNode(1, 0, 0, 0, 0, 10000, 0, 0);
-		SimulationNode node2 = new SimulationNode(2, 0, 0, 0, 10000, 10000, 0, 0);
-		SimulationNode node3 = new SimulationNode(3, 0, 0, 0, 0, 20000, 0, 0);
-
-		graphBuilder.addNode(node0);
-		graphBuilder.addNode(node1);
-		graphBuilder.addNode(node2);
-		graphBuilder.addNode(node3);
-
-		SimulationEdge edge1 = new SimulationEdge(node0, node1, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node0, node1)));
-		SimulationEdge edge2 = new SimulationEdge(node1, node0, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node1, node0)));
-		SimulationEdge edge3 = new SimulationEdge(node1, node2, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node1, node2)));
-		SimulationEdge edge4 = new SimulationEdge(node2, node1, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node2, node1)));
-		SimulationEdge edge5 = new SimulationEdge(node1, node3, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node1, node3)));
-		SimulationEdge edge6 = new SimulationEdge(node3, node1, 0, 0, 100, 40, 1, new EdgeShape(Arrays.asList(node3, node1)));
-
-		graphBuilder.addEdge(edge1);
-		graphBuilder.addEdge(edge2);
-		graphBuilder.addEdge(edge3);
-		graphBuilder.addEdge(edge4);
-		graphBuilder.addEdge(edge5);
-		graphBuilder.addEdge(edge6);
-
-		Graph<SimulationNode, SimulationEdge> graph = graphBuilder.createGraph();
 
 		Trip<SimulationNode>[] trips = new Trip[20];
 
