dba6ca0385ac3ba35de5745365cf2d60f41c66da
David Fiedler <david.fido.fiedler@gmail.com>
2024-02-07 23:35:45 +0100

    version raised, failing test excluded; heterogenous vehicle related support

---
+++ src/main/java/cz/cvut/fel/aic/agentpolis/simmodel/entity/vehicle/SimpleTransportVehicle.java
@@ -0,0 +1,44 @@
+package cz.cvut.fel.aic.agentpolis.simmodel.entity.vehicle;
+
+import cz.cvut.fel.aic.agentpolis.simmodel.entity.EntityType;
+import cz.cvut.fel.aic.agentpolis.simmodel.entity.TransportableEntity;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.GraphType;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
+
+/**
+ * Simple implementation of transport vehicle with homogeneous transport slots
+ */
+public class SimpleTransportVehicle<T extends TransportableEntity> extends PhysicalTransportVehicle<T> {
+
+    private final int vehiclePassengerCapacity; // number of passenger, including driver
+
+
+
+    public int getCapacity() {
+        return vehiclePassengerCapacity;
+    }
+
+
+    public SimpleTransportVehicle(
+        int vehiclePassengerCapacity,
+        String vehicleId,
+        EntityType type,
+        float lengthInMeters,
+        GraphType usingGraphTypeForMoving,
+        SimulationNode position,
+        int maxVelocity
+    ) {
+        super(vehicleId, type, lengthInMeters, usingGraphTypeForMoving, position, maxVelocity);
+        this.vehiclePassengerCapacity = vehiclePassengerCapacity;
+    }
+
+    @Override
+    boolean canTransport(T entity) {
+        return false;
+    }
+
+    @Override
+    boolean hasCapacityFor(T entity) {
+        return getCapacity() > getTransportedEntities().size();
+    }
+}
